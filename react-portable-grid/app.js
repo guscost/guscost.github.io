cb4909107479fa3cea742f49f9b6efdb903effb4
guscost <guscost@gmail.com>
2015-12-12 21:27:39 -0500

    expanding detail rows

--- react-portable-grid/app.js
+++ react-portable-grid/app.js
@@ -1,63 +1,75 @@
 // app component
 var PortableGridTest = React.createClass({
 
-	displayName: "PortableGridTest",
+    displayName: "PortableGridTest",
 
-	getInitialState: function () {
-		return {    
-			data: testData,             
-			columns: [{
-				field: "firstName",
-				title: "First Name",
-				width: "20%"
-			},{
-				field: "lastName",
-				title: "Last Name",
-				width: "20%"
-			},{
-				field: "email",
-				title: "Email Address",
-				width: "40%"
-			},{
-				title: "Birthday",
-				width: "15%",
-				template: function (item) {
-					return moment(item.birthday).format("MMM D");				
-				}
-			},{
-				title: " ",
-				width: "5%",
-				align: "center",
-				template: function (item) {
-					return <a href="#" onClick={this._onClickRowDetails.bind(this, item)}>
-						<span className="glyphicon glyphicon-info-sign"></span>
-					</a>;				
-				}
-			}],
-			currentPage: 2
-		};
-	},
+    getInitialState: function () {
+        return {    
+            data: testData,
+            currentPage: 1
+        };
+    },
 
-	_onChangePage: function (value) {
-		this.setState({ currentPage: value })
-	},	
-	
-	_onClickRowDetails: function (item) {
-		alert(item.summary);
-	},
-	
-	render: function () {
-		return <div className="col-xs-9">
-			<h2>Portable Grid Demo</h2>
-			<PortableGrid 
-				parent={this}
-				data={this.state.data}
-				columns={this.state.columns}
-				currentPage={this.state.currentPage}
-				pageSize={12}
-				onChangePage={this._onChangePage} />
-		</div>;
-	}
+    _onChangePage: function (value) {
+        this.setState({ currentPage: value })
+    },    
+    
+    _onClickRowDetails: function (item) {
+        item._rowSelected = !item._rowSelected;
+        this.forceUpdate();
+    },
+    
+    render: function () {
+        return <div className="col-xs-9">
+            <h2>Portable Grid Demo</h2>
+            <PortableGrid 
+                scope={this}
+                data={this.state.data}
+                columns={[{
+                    field: "firstName",
+                    title: "First Name",
+                    width: "20%"
+                },{
+                    field: "lastName",
+                    title: "Last Name",
+                    width: "20%"
+                },{
+                    field: "email",
+                    title: "Email Address",
+                    width: "40%"
+                },{
+                    title: "Birthday",
+                    width: "15%",
+                    template: function (item) {
+                        return moment(item.birthday).format("MMM D");                
+                    }
+                },{
+                    title: " ",
+                    width: "5%",
+                    align: "center",
+                    template: function (item) {
+                        return <a href="#" onClick={this._onClickRowDetails.bind(this, item)}>
+                            <span className="glyphicon glyphicon-info-sign"></span>
+                        </a>;                
+                    }
+                }]}
+                details={function (item) {                
+                    return <div className="form form-horizontal" style={{ paddingTop: 10 }}>
+                        <div className="form-group">
+                            <label className="col-xs-2 control-label">Zip Code:</label>
+                            <div className="col-xs-10 form-control-static">{item.zipCode}</div>
+                        </div>
+                        <div className="form-group">
+                            <label className="col-xs-2 control-label">Summary:</label>
+                            <div className="col-xs-10 form-control-static">{item.summary}</div>
+                        </div>
+                    </div>;
+                }}
+                currentPage={this.state.currentPage}
+                onChangePage={this._onChangePage}
+                pageSize={12} />
+        </div>;
+    }
 });
 
 // start it up
